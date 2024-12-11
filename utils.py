74896b3638ee23a63aab2f0e8d0c6fc565fe7a83
kooqooo <koogmlcks@gmail.com>
2024-12-11 17:15:19 +0900

    feat: streaming 방식의 채팅을 지원하는 함수 추가

---
+++ utils.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+from config import openai
+
+
+def write_message(message: dict[str, str]):
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+def write_messages(messages: list[dict[str, str]]):
+    for message in messages:
+        write_message(message)
+
+
+def write_input():
+    return st.text_input("메시지 입력", key="input")
+
+
+def write_stream_message(message: dict[str, str]):
+    with st.chat_message("assistant"):
+        st.write_stream(message["content"])
+
+
+def get_response_stream(messages: list[dict[str, str]]):
+    response = openai.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages,
+        stream=True,
+    )
+    
+    full_response = "" # 전체 응답을 저장할 변수
+
+    # 스트리밍 응답 처리하여 yield 반환
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+            content = chunk.choices[0].delta.content
+            full_response += content
+            yield content
+
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
